3eef5d16e2cd2b86fad96cdfd1c633c3c5a0da52
Antwhale <olollolo339@naver.com>
2025-01-11 21:31:31 +0900

    Initial Commit

---
+++ SampleNetworkingWithCombine/SampleNetworkingWithCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleNetworkingWithCombineApp.swift
+//  SampleNetworkingWithCombine
+//
+//  Created by 부재식 on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleNetworkingWithCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
